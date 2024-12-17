152c44da559fe138b27fd2f7e62b33b6670d7395
Greeshman <bunnymadgamer@gmail.com>
2024-12-17 20:05:38 +0530

    intial commit

---
+++ main3.js
@@ -0,0 +1,26 @@
+function myDisplayer(sum)
+{
+    document.getElementById("output").innerHTML = sum;
+}
+function sum(a,b)
+{
+    var sum = a + b;
+    myDisplayer(sum);
+}
+let myPromise = new Promise(function(myResolve,myReject)
+{
+    let x = 0;
+    if (x == 0)
+    {
+        myResolve("OK");
+    }
+    else
+    {
+        myReject("Error");
+    }
+});
+myPromise.then
+(
+    function(value) {sum(5,5);},
+    function(error) {sum(error);}
+);
---
+++ main4.js
@@ -0,0 +1,10 @@
+function myDisplayer(value)
+{
+    document.getElementById("result").innerHTML = value;
+}
+function myCalculator(num1 , num2 , myCallback)
+{
+    let sum = num1 + num2;
+    myCallback(sum);
+}
+myCalculator(5 , 5 , myDisplayer);
